cd85e970a6685357b3a41382a9ffa45f2164a63a
SeongmoAhn <asm0619@gmail.com>
2022-12-29 12:20:47 +0900

    ArrayStack v1

---
+++ stack/arraystack/ArrayStack.h
@@ -0,0 +1,53 @@
+#include <cstdio>
+#include <cstdlib>
+const int MAX_STACK_SIZE = 20;
+
+class ArrayStack
+{
+    int top;
+    int data[MAX_STACK_SIZE];
+
+public:
+    ArrayStack() { top = -1; }
+    ~ArrayStack() {}
+    bool isEmpty() { return top == -1; }
+    bool isFull() { return top == (MAX_STACK_SIZE - 1); }
+
+    void push(int x)
+    {
+        if (isFull())
+        {
+            fprintf(stderr, "Stack is Full\n");
+            exit(1);
+        }
+        data[++top] = x;
+    }
+
+    int pop()
+    {
+        if (isEmpty())
+        {
+            fprintf(stderr, "Stack is Empty\n");
+            exit(1);
+        }
+        return data[top--];
+    }
+
+    int peek()
+    {
+        if (isEmpty())
+        {
+            fprintf(stderr, "Stack is Empty\n");
+            exit(1);
+        }
+        return data[top];
+    }
+
+    void display()
+    {
+        printf("[Number of Stack elements : %d]\n", top + 1);
+        for (int i = 0; i <= top; i++)
+            printf("<%2d>", data[i]);
+        printf("\n\n");
+    }
+};
--- stack/arraystack/main.cpp
+++ stack/arraystack/main.cpp
@@ -1,5 +1,22 @@
-#include <cstdio>
+#include "ArrayStack.h"
+
 int main(void)
 {
-    printf("hello\n");
+    ArrayStack stack;
+    for (int i = 0; i < 10; i++)
+        stack.push(i);
+    stack.display();
+    stack.pop();
+    stack.pop();
+    stack.pop();
+    stack.display();
+
+    return 0;
 }
+/*
+[Number of Stack elements : 10]
+< 0>< 1>< 2>< 3>< 4>< 5>< 6>< 7>< 8>< 9>
+
+[Number of Stack elements : 7]
+< 0>< 1>< 2>< 3>< 4>< 5>< 6>
+*/
